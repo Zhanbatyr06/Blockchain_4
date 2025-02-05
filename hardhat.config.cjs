95b5abfc6968e5494edb6e9fc86fe55049e7975d
Zhanbatyr06 <zhanbatyrmolkryt@gmail.com>
2025-02-05 17:29:08 +0500

    marketplace

---
+++ hardhat.config.cjs
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545", // Default Ganache URL
+      // You can get a private key from one of your Ganache accounts
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.PROCESS_API}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: `${process.env.PROCESS_API}`,
+  },
+};
